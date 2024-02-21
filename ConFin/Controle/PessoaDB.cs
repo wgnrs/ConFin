b5157fe1408ad7a71463a708ac67d339b1e9e1a1
Wagner <wagner_silvajr@hotmail.com>
2024-02-21 15:00:35 -0300

    feat: criado classe pessoadb e metodo getpessoas

---
+++ ConFin/Controle/PessoaDB.cs
@@ -0,0 +1,46 @@
+﻿using ConFin.Modelo;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConFin.Controle
+{
+    public class PessoaDB
+    {
+        public static List<Pessoa> GetPessoas(NpgsqlConnection conexao)
+        {
+            List<Pessoa> lista = new List<Pessoa>();
+            try
+            {
+                string sql = "select * from pessoa oder by nome";
+                NpgsqlCommand cdm = new NpgsqlCommand(sql, conexao);
+                NpgsqlDataReader rd = cdm.ExecuteReader();
+                while (rd.Read())
+                {
+                    int pessoaId = int.Parse(rd["pessoaId"].ToString());
+                    string nome = rd["nome"].ToString();
+                    string cpfcnpj = rd["cpfcnpj"].ToString();
+                    string endereco = rd["endereco"].ToString();
+                    string bairro = rd["bairro"].ToString();
+                    string telefone = rd["telefone"].ToString();
+                    string email = rd["email"].ToString();
+                    int tipo = int.Parse(rd["tipo"].ToString());
+                    int cidadeId = int.Parse(rd["cidadeId"].ToString());
+                    Pessoa pessoa = new Pessoa(pessoaId, nome, cpfcnpj, endereco, bairro, telefone, email, tipo, cidadeId);
+                    lista.Add(pessoa);
+
+                }
+            }
+            catch (NpgsqlException erro)
+            {
+                MessageBox.Show("Erro de SQL: " + erro.Message);                
+            }
+            return lista;
+        }
+    }
+}
