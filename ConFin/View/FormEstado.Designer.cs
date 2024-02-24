ae0f25e805a946ceee7ba8b4d507fb517f4110ed
Wagner <wagner_silvajr@hotmail.com>
2024-02-24 09:18:10 -0300

    feat: novo icone no formulario estado

--- ConFin/View/FormEstado.Designer.cs
+++ ConFin/View/FormEstado.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormEstado));
             this.SuspendLayout();
             // 
             // FormEstado
@@ -35,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormEstado";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Cadastro de estado";
