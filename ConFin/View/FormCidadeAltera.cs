6b148efa95907f93c88c2f3d9c43ab8d728845cc
Wagner <wagner_silvajr@hotmail.com>
2024-02-28 18:24:37 -0300

    feat: populando o formalterar

--- ConFin/View/FormCidadeAltera.cs
+++ ConFin/View/FormCidadeAltera.cs
@@ -1,4 +1,5 @@
-﻿using ConFin.Modelo;
+﻿using ConFin.Controle;
+using ConFin.Modelo;
 using Npgsql;
 using System;
 using System.Collections.Generic;
@@ -21,11 +22,32 @@ namespace ConFin.View
             this.conexao = conexao;
             this.cidade = cidade;
             InitializeComponent();
+            textBoxNome.Text = cidade.nome;
+            textBoxId.Text = cidade.cidadeId.ToString();
+            PopulaComboEstado();
         }
 
         private void Label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void PopulaComboEstado()
+        {
+            List<Estado> lista = EstadoDB.GetEstados(conexao);
+            comboBoxEstado.Items.Clear();
+            comboBoxEstado.DataSource = lista;
+            int indice = 0;
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Estado estado = lista[i];
+                if (estado.estadoSigla == cidade.estadoSigla)
+                {
+                    indice = i;
+                    break;
+                }
+            }
+            comboBoxEstado.SelectedIndex = indice;
         }
 
         private void buttonCancelar_Click(object sender, EventArgs e)
